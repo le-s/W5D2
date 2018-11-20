c7b21ec230a8c0bd07cc8f0860e3a439eb7d7732
le-s <steven.an.le@gmail.com>
2018-11-20 13:51:00 -0800

    Create user and session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      # redirect_to
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+end
---
+++ test/models/girlfriend_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GirlfriendTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
