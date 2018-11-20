e003d63a540f6aab252856d2a3ee53321b511431
le-s <steven.an.le@gmail.com>
2018-11-20 14:03:45 -0800

    Write subs controller expeditiously

---
+++ app/models/sub.rb
@@ -0,0 +1,8 @@
+class Sub < ApplicationRecord
+  validates :title, :description, :moderator_id, presence: true
+
+  belongs_to :moderator,
+    foreign_key: :moderator_id,
+    primary_key: :id,
+    class_name: :User
+end
